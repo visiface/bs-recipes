c6416ec0ea771031424e357e6686d985720025de
visi <visiface@live.com>
2021-09-20 18:12:28 +0200

    scss practice

--- template-parts/content-recipes.php
+++ template-parts/content-recipes.php
@@ -8,29 +8,66 @@
  */
 
 ?>
-    <div class="content-container">
-        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-            <header class="entry-header">
-                <?php
-                if ( is_singular() ) :
-                    the_title( '<h1 class="entry-title">', '</h1>' );
-                else :
-                    the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-                endif;
 
-                if ( 'post' === get_post_type() ) :
-                    ?>
-                    <div class="entry-meta">
-                        <?php
-                        bs_recipes_posted_on();
-                        bs_recipes_posted_by();
-                        ?>
-                    </div><!-- .entry-meta -->
-                <?php endif; ?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <div class="content-container">
+        <div class="intro-container">
+            <header class="entry-header">
+                <div class="recipe-title">
+                    <?php
+                        if ( is_singular() ) :
+                            the_title( '<h1 class="entry-title">', '</h1>' );
+                        else :
+                            the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+                        endif;
+
+                        if ( 'post' === get_post_type() ) :
+                            ?>
+                            <div class="entry-meta">
+                                <?php
+                                bs_recipes_posted_on();
+                                bs_recipes_posted_by();
+                                ?>
+                            </div><!-- .entry-meta -->
+                        <?php endif; ?>
+                </div>
             </header><!-- .entry-header -->
 
-    
-        <?php bs_recipes_post_thumbnail(); ?>
+            <div class="note-container">
+                <div class="notes">
+                    <h2>Author's Notes:</h2>
+                    <?php
+                        foreach ( get_field('authornote') as $authornote ) { ?>
+                            <p>
+                                <?= $authornote['note']; ?>
+                            </p>
+                        <?php 
+                        }
+                    ?>
+                </div>
+            </div>
+
+            <div class="the-content">
+                <?php
+                    the_content(
+                        sprintf(
+                            wp_kses(
+                                /* translators: %s: Name of current post. Only visible to screen readers */
+                                __( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'bs-recipes' ),
+                                array(
+                                    'span' => array(
+                                        'class' => array(),
+                                    ),
+                                )
+                            ),
+                            wp_kses_post( get_the_title() )
+                        )
+                    );
+
+                    bs_recipes_post_thumbnail(); 
+                ?>
+            </div>
+        </div>
         
         <div class="recipe-container">
             <div class="ingredients">
@@ -58,35 +95,21 @@
                             </li>
                         <?php } ?>
                     </ol>
+                </div>
             </div>
         </div>
-    </div>
 
-	<div class="entry-content">
-		<?php
-		the_content(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'bs-recipes' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				wp_kses_post( get_the_title() )
-			)
-		);
-
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'bs-recipes' ),
-				'after'  => '</div>',
-			)
-		);
-		?>
-	</div><!-- .entry-content -->
+        <div class="entry-content">
+            <?php
+            
+            wp_link_pages(
+                array(
+                    'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'bs-recipes' ),
+                    'after'  => '</div>',
+                )
+            );
+            ?>
+        </div><!-- .entry-content -->
 
 	<footer class="entry-footer">
 		<?php bs_recipes_entry_footer(); ?>
