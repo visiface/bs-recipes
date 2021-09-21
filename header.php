7d56df11deb5ec344f3d88b523e91c198ee66580
visi <visiface@live.com>
2021-09-21 18:49:53 +0200

    header adjustments

--- header.php
+++ header.php
@@ -23,24 +23,22 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'bs-recipes' ); ?></a>
-
-	<header id="masthead" class="site-header">
+    <header id="masthead" class="site-header">
 		<div class="site-branding">
 			<?php
-			the_custom_logo();
+			    the_custom_logo();
 			?>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'bs-recipes' ); ?></button>
 			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
+                wp_nav_menu(
+                    array(
+                        'theme_location' => 'menu-1',
+                        'menu_id'        => 'primary-menu',
+                    )
+                );
 			?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
