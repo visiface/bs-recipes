40591a61bbba0137f537bec68130589983d35a33
visi <visiface@live.com>
2021-09-21 22:20:05 +0200

    archive formatting

--- template-parts/content-bs_recipie-archive.php
+++ template-parts/content-bs_recipie-archive.php
@@ -12,38 +12,42 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <div class="content-container">
         <div class="archive-container">
-        
-        </div>
-        
-        
-        
-        <div class="intro-container">
-            <header class="entry-header">
-                <div class="recipe-title">
-                    <?php
-                        if ( is_singular() ) :
-                            the_title( '<h1 class="entry-title">', '</h1>' );
-                        else :
-                            the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-                        endif;
-
-                        if ( 'post' === get_post_type() ) :
-                            ?>
-                            <div class="entry-meta">
-                                <?php
-                                bs_recipes_posted_on();
-                                bs_recipes_posted_by();
-                                ?>
-                            </div><!-- .entry-meta -->
-                        <?php endif; ?>
-                </div>
-            </header><!-- .entry-header -->
-
+            <div class="archive-img">
                 <?php
                     bs_recipes_post_thumbnail(); 
                 ?>
             </div>
+
+            <div class="intro-container">
+                <header class="entry-header">
+                    <div class="recipe-title">
+                        <?php
+                            if ( is_singular() ) :
+                                the_title( '<h1 class="entry-title">', '</h1>' );
+                            else :
+                                the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+                            endif;
+
+                            if ( 'post' === get_post_type() ) :
+                                ?>
+                                <div class="entry-meta">
+                                    <?php
+                                    bs_recipes_posted_on();
+                                    bs_recipes_posted_by();
+                                    ?>
+                                </div><!-- .entry-meta -->
+                            <?php endif; ?>
+                    </div>
+                </header><!-- .entry-header -->
+
+                    
+                </div>
+            </div>
         </div>
+        
+        
+        
+        
 
         
 </article><!-- #post-<?php the_ID(); ?> -->
