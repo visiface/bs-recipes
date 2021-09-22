50cbda74a8e7069a872fe0eac8cb7b35aeda94d0
visi <visiface@live.com>
2021-09-22 16:21:28 +0200

    footer changes and styling

--- footer.php
+++ footer.php
@@ -22,7 +22,7 @@
 			<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'bs-recipes' ), 'bs-recipes', '<a href="http://underscores.me/">Underscores.me</a>' );
+				printf( esc_html__( 'Theme: %1$s by Kim using %2$s!', 'bs-recipes' ), 'bs-recipes', '<a href="http://underscores.me/">Underscores.me</a>' );
 				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
